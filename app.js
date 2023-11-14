edf0da0cbf9ab33c758a08d800888dfdc83f54f3
Philip <philiposun@gmail.com>
2023-11-14 01:04:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ let iconCart = document.querySelector('.icon-cart');
 let closeCart = document.querySelector('.close');
 let body = document.querySelector('body');
 let listProductHTML = document.querySelector('.listProduct');
-let listCartHTML = document.querySelector('')
+let listCartHTML = document.querySelector('.listCart');
+let iconCartSpan = document.querySelector('.icon-cart span');
 
 let listProducts = [];
 let carts = [];
