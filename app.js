41b5af60a0139898ea634988d79acc29bdccae28
Philip <philiposun@gmail.com>
2023-11-28 01:18:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const addDataToHTML = () => {
                     Add To Cart
                 </button>
             `;
+            
         listProductHTML.appendChild(newProduct);
         })
     }
